a39c6c996748d8e7d669191c9729f5d6140f8ef4
tojan-bukhari <gitn12333@gmail.com>
2020-12-16 20:32:26 +0200

    all setup

--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,22 @@ var express = require('express');
 var morgan = require('morgan')
 var cors = require('cors')
 var app = express();
-const mongoose = require('mongoose');
+
 require('dotenv').config();
+
 app.use(cors())
 app.use(express.json()); 
+
+const mongoose = require('mongoose');
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
 );
+
 const connection = mongoose.connection;
 connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
 });
+
 //port with what ever the port will be given by heruko
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
