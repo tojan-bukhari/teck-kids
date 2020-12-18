99f12a17e9fff94cc1298ca061b28e78d13f828c
TahanyAljemzawy <tahanyaljemzawy@yahoo.com>
2020-12-18 20:42:54 +0200

    getHtmlbyID

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,12 @@ var morgan = require('morgan')
 var cors = require('cors')
 var app = express();
 
+const courseRoute = require('./routers/courseRoute');
 require('dotenv').config();
 
 app.use(cors())
 app.use(express.json()); 
+app.use(morgan('dev'));
 
 const mongoose = require('mongoose');
 const uri = process.env.ATLAS_URI;
@@ -17,6 +19,9 @@ const connection = mongoose.connection;
 connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
 });
+
+//the routes 
+app.use('/course',courseRoute)
 
 //port with whatever the port will be given by heruko
 const port = process.env.PORT || 8000;
