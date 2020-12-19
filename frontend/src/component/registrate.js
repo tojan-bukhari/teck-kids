f3b430485c5551d8c6b7f82b2b8ede1843b48677
rahmeh alomari <rahmehalomari577@gmail.com>
2020-12-19 22:17:48 +0200

    test

--- frontend/src/component/registrate.js
+++ frontend/src/component/registrate.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 import { useHistory } from "react-router-dom";
 
 /****************************************************/
-
+///////////////tojaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaan
 //use the arrow function to bind values 
 const Create = ()=>{
     const history = useHistory();
