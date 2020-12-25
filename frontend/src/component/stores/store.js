fb722ab4bbb161c5eb9c94cec04adecb7411a646
rahmeh alomari <rahmehalomari577@gmail.com>
2020-12-25 10:56:36 +0200

    style

--- frontend/src/component/stores/store.js
+++ frontend/src/component/stores/store.js
@@ -2,10 +2,12 @@ import { createStore, combineReducers } from 'redux'
 import { reducer as formReducer } from 'redux-form'
 
 const rootReducer = combineReducers({
-    // ...your other reducers here
-    // you have to pass formReducer under 'form' key,
-    // for custom keys look up the docs for 'getFormState'
-    form: formReducer
+     
+
+
+
+
+    
   })
   
   const store = createStore(rootReducer)
