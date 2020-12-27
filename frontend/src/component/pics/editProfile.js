c2e70367caa411e47de502146615354cce7abda7
tojan-bukhari <gitn12333@gmail.com>
2020-12-27 13:21:57 +0200

    class name warninig- solved

--- frontend/src/component/pics/editProfile.js
+++ frontend/src/component/pics/editProfile.js
@@ -2,12 +2,6 @@ import React, { Component } from 'react';
 import axios from "axios";
 import "antd/dist/antd.css";
 
-// import { storage } from "./firebase.js";
-// import Footer from './Footer';
-// import Navbar_login from "./Navbar_Login"
-
-//
-=======
 
 
 
