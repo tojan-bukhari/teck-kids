8c8559f309e090ebf2dfd22c4da847156897f14a
hello <rahmeh19996@gmail.com>
2020-12-28 22:06:21 +0200

    make it better

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,10 @@ var express = require('express');
 var morgan = require('morgan')
 var cors = require('cors')
 var app = express();
+const path = require('path');
 
-//the Routes
+
+//the Routes...
 const authRoutes = require('./routes/auth');
 const courseRoute = require('./routes/courseRoute');
 const userRoute=require('./routes/userRoute')
@@ -14,6 +16,22 @@ app.use(express.json());
 ///
 
 app.use(morgan('dev'));
+
+// --> Add this
+// ** MIDDLEWARE ** //
+const whitelist = ['http://localhost:3000', 'http://localhost:8080']
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin)
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable")
+      callback(null, true)
+    } else {
+      console.log("Origin rejected")
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
 
 const mongoose = require('mongoose');
 const uri = process.env.ATLAS_URI;
@@ -30,6 +48,15 @@ connection.once('open', () => {
 app.use('/api',authRoutes);
 app.use('/course',courseRoute);
 app.use('/user',userRoute);
+// --> Add this
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'frontend/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+  });
+}
 
 //port with whatever the port will be given by heruko
 const port = process.env.PORT || 8000;
