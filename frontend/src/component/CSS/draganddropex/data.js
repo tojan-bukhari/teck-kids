67195feccf160319563fef835f14eca223865c6e
= <=>
2020-12-29 19:08:03 +0200

    firebaseStartUp

--- frontend/src/component/CSS/draganddropex/data.js
+++ frontend/src/component/CSS/draganddropex/data.js
@@ -27,18 +27,6 @@ var data = [
 
 function Data() {
 
-//   var locals =  localStorage.getItem('List')
-// console.log(JSON.parse(locals)[1].items, 'hpppppppppi') 
-//   // console.log(locals[0], 'indexesssss') 
-
-//   // var mydata = JSON.stringify(rightData[1])
-//   console.log(rightData[1].items,'rightData')
-//   // if (!data)console.log('bass')
-// // console.log(rightData[1].items, "mesh hayyy") 
-//      if(rightData[1].items === JSON.parse(locals)[1].items){
-//       alert("passssssssssss");
-//       // window.location('/')
-//           }
 
   return (
     <div className="defaultData"> 
@@ -50,30 +38,5 @@ function Data() {
 }
 
 export default Data
-
-
-// function Data() {
-  
-//   var locals =  localStorage.getItem('List')
-//   console.log({locals}, 8800) 
-//   var mydata = JSON.stringify(rightData[1])
-//   console.log(locals)
-  
-// console.log(rightData[1].items, "mesh hayyy")
-//             if(JSON.stringify(rightData[1].items) === JSON.stringify({locals})){
-//              console.log(JSON.stringify(rightData[1]),"               compare                   " ,  locals.items)
-//           }
-
-//   return (
-//     <div className="defaultData"> 
-//       <header className="defaultData-header"> 
-//       <DragNDrop data = {data}/>
-//       </header>
-//     </div>
-//   )
-// }
-
-// export default Data
-
 
 
