73a9a66750e2aa6402f76056feb253420765b715
hello <rahmeh19996@gmail.com>
2021-01-02 09:22:42 +0200

    chatrahmeh

--- backend/users.js
+++ backend/users.js
@@ -1,7 +1,7 @@
 const users = [];
 
 const addUser = ({ id, name, room }) => {
-//
+
   name = name
   room = room.trim().toLowerCase();
   const existingUser = users.find((user) => user.room === room && user.name === name);
