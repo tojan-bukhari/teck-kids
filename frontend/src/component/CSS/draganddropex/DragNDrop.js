69b1183790eaffc45fcf7f0361d7dbb18baac3c0
Emanhusseinn <eman.hussein.jo@gmail.com>
2021-01-05 13:00:52 +0200

    dndcssex2-done

--- frontend/src/component/CSS/draganddropex/DragNDrop.js
+++ frontend/src/component/CSS/draganddropex/DragNDrop.js
@@ -70,59 +70,62 @@ function DragNDrop({data}) {
 
 
     return (
-        <div className="drag-n-drop">
-        {list.map((grp, grpI) =>( 
-           <div key={grp.title}
-            className="dnd-group"
-            onDragEnter={dragging && !grp.items.length?(e) => handleDragEnter(e,{grpI, itemI: 0}):null}
-            
-            >
-             <div className="group-title">{grp.title}</div>
-              {grp.items.map((item, itemI) => (
-                <div draggable   
-                 onDragStart={(e) => {handletDragStart(e, {grpI, itemI})} }
-                 onDragEnter={dragging?(e) => {handleDragEnter(e, {grpI, itemI})}:null} 
-                      key = {item} 
-                       className={dragging?getStyles({grpI, itemI}):"dnd-item"}
-                       
-                       >
-              {item}
-                  </div>
-              ))}
-           </div>
-        ))} 
-        
-        <div>
-
-
-        {localStorage.getItem('List')===  '[{"title":"Drag","items":[]},{"title":"Drop here","items":["p {","text-align: center;"," color: red;","}"]}]' ||localStorage.getItem('List')===  '[{"title":"Drag","items":[]},{"title":"Drop here","items":["p {"," color: red;","text-align: center;","}"]}]'? <div>  
-           <Button variant="primary" onClick={handleShow}> SUBMIT </Button>
-
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Woohoo!!!!</Modal.Title>
-        </Modal.Header>
-        <img src= "https://www.flaticon.com/svg/static/icons/svg/3159/3159066.svg" alt="css"/>
-        <Modal.Footer>
+      <div id= 'bbbbbb'>   
+                     <p  id='ididid'> ▶▶▶move the items from the Drag Bar and drop them in the right order inside the Drop bar◀◀◀</p>
+<div className="drag-n-drop">
+      {list.map((grp, grpI) =>( 
+         <div key={grp.title}
+          className="dnd-group"
+          onDragEnter={dragging && !grp.items.length?(e) => handleDragEnter(e,{grpI, itemI: 0}):null}
           
-          <Button variant="primary" onClick={()=>{history.push('/CSS/ex3')}}> Go to the Next exersise </Button>
-        </Modal.Footer>
-      </Modal></div>:<div>  
-           <Button variant="primary" onClick={handleShow}> SUBMIT </Button>
+          >
+           <div className="group-title">{grp.title}</div>
+            {grp.items.map((item, itemI) => (
+              <div draggable   
+               onDragStart={(e) => {handletDragStart(e, {grpI, itemI})} }
+               onDragEnter={dragging?(e) => {handleDragEnter(e, {grpI, itemI})}:null} 
+                    key = {item} 
+                     className={dragging?getStyles({grpI, itemI}):"dnd-item"}
+                     
+                     >
+            {item}
+                </div>
+            ))}
+         </div>
+      ))} 
+      
+      <div>
 
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Try Again!!!!</Modal.Title>
-        </Modal.Header>
-        <img src= "https://www.flaticon.com/svg/static/icons/svg/3159/3159020.svg" alt="css"/>
-        <Modal.Footer>
-        </Modal.Footer>
-      </Modal></div>}
 
-        </div>
+      {localStorage.getItem('List')===  '[{"title":"Drag","items":[]},{"title":"Drop here","items":["p {","text-align: center;"," color: red;","}"]}]' ||localStorage.getItem('List')===  '[{"title":"Drag","items":[]},{"title":"Drop here","items":["p {"," color: red;","text-align: center;","}"]}]'? <div>  
+         <Button variant="primary" onClick={handleShow}> SUBMIT </Button>
 
-  
-  </div>
+    <Modal show={show} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Woohoo!!!!</Modal.Title>
+      </Modal.Header>
+      <img src= "https://www.flaticon.com/svg/static/icons/svg/3159/3159066.svg" alt="css"/>
+      <Modal.Footer>
+        
+        <Button variant="primary" onClick={()=>{history.push('/CSS/ex3')}}> Go to the Next exersise </Button>
+      </Modal.Footer>
+    </Modal></div>:<div>  
+         <Button variant="primary" onClick={handleShow}> SUBMIT </Button>
+
+    <Modal show={show} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Try Again!!!!</Modal.Title>
+      </Modal.Header>
+      <img src= "https://www.flaticon.com/svg/static/icons/svg/3159/3159020.svg" alt="css"/>
+      <Modal.Footer>
+      </Modal.Footer>
+    </Modal></div>}
+
+      </div>
+
+
+</div></div>
+      
   
     )
 }
