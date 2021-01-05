f11804b828f5c1124e23d18201c54a3fa10bfd49
Emanhusseinn <eman.hussein.jo@gmail.com>
2021-01-06 00:11:13 +0200

    cssex3

--- frontend/src/component/HTML/draganddropex/DragNDrop.js
+++ frontend/src/component/HTML/draganddropex/DragNDrop.js
@@ -1,27 +1,27 @@
-
 import React, {useState, useRef} from 'react'
 import { Button, Modal} from 'react-bootstrap';
 import { useHistory } from "react-router-dom";
+// import './momo.css';
 
 
-function DragNDrop({data, rightData}) {
+
+
+/*************************************************************** */
+function DragNDrop({data}) {
+
   const history = useHistory();
-
     const [list, setList] = useState(data); 
     const [dragging, setDragging] = useState(false);
     const [show, setShow] = useState(false);
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
-  
-
     const dragItem = useRef();
         const dragItemNode = useRef();
         
     const handletDragStart = (e, params) => {
                 console.log('dragging', params)
                 dragItem.current = params;
-                // console.log(current)
                 dragItemNode.current = e.target;
                 dragItemNode.current.addEventListener('dragend', handleDragEnd)
         
@@ -57,89 +57,79 @@ function DragNDrop({data, rightData}) {
                             dragItemNode.current = null;
                         }
 
-                
+
+                  
+
                 const getStyles = (params) => {
                     const currentItem =  dragItem.current;
                 if (currentItem.grpI === params.grpI && currentItem.itemI === params.itemI) {
                     return ' dnd-item current'
                             }
-                            return "dnd-item"
+                            return "dnd-item" 
+                    
+                    
                         }
 
 
-                        const handleClick= ()=> {
-                          const locals =  localStorage.getItem('List')
-                          const localData =  JSON.parse(locals)[1].items 
-                         const mydata = rightData[1].items
-                     
-                            for (var i = 0; i < localData.length; i++) {
-                              for (var j = 0; j < mydata.length; j++) {
-                                  if (localData[i] === mydata[j] && localData.length> 1) {
-                                    console.log(mydata, "MY DATA") 
-                              console.log(localData, "LOCAL'S STORAGEDATA")
-                              
-                              return alert("greate job");
-                                   
-                                  }else return alert('try again')
-                              }
-                          }
-    
-                 
-                            }
-
-
-
     return (
-        <div className="drag-n-drop">
-        {list.map((grp, grpI) =>( 
-           <div key={grp.title}
-            className="dnd-group"
-            onDragEnter={dragging && !grp.items.length?(e) => handleDragEnter(e,{grpI, itemI: 0}):null}
-            
-            >
-             <div className="group-title">{grp.title}</div>
-              {grp.items.map((item, itemI) => (
-                <div draggable   
-                 onDragStart={(e) => {handletDragStart(e, {grpI, itemI})} }
-                 onDragEnter={dragging?(e) => {handleDragEnter(e, {grpI, itemI})}:null} 
-                      key = {item} 
-                       className={dragging?getStyles({grpI, itemI}):"dnd-item"}
-                       
-                       >
-              {item}
-                  </div>
-              ))}
-           </div>
-        ))}    
-          <div>
-        <Button variant="primary" onClick={handleShow}>
-SUBMIT      </Button>
+      <div id= 'bbbbbb'>   
+                     <p  id='ididid'> ▶▶▶move the items from the Drag Bar and drop them in the right order inside the Drop bar◀◀◀</p>
+<div className="drag-n-drop">
+      {list.map((grp, grpI) =>( 
+         <div key={grp.title}
+          className="dnd-group"
+          onDragEnter={dragging && !grp.items.length?(e) => handleDragEnter(e,{grpI, itemI: 0}):null}
+          
+          >
+           <div className="group-title">{grp.title}</div>
+            {grp.items.map((item, itemI) => (
+              <div draggable   
+               onDragStart={(e) => {handletDragStart(e, {grpI, itemI})} }
+               onDragEnter={dragging?(e) => {handleDragEnter(e, {grpI, itemI})}:null} 
+                    key = {item} 
+                     className={dragging?getStyles({grpI, itemI}):"dnd-item"}
+                     
+                     >
+            {item}
+                </div>
+            ))}
+         </div>
+      ))} 
+      
+      <div>
 
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Woohoo!!!!</Modal.Title>
-        </Modal.Header>
+
+      {localStorage.getItem('List')===  '[{"title":"Drag","items":[]},{"title":"Drop here","items":["<!DOCTYPE html>","<html>","<body>","<p>My paragraph.</p>","<h1>My Heading</h1>","</body>","</html>"]}]' ||localStorage.getItem('List')===  '[{"title":"Drag","items":[]},{"title":"Drop here","items":["<!DOCTYPE html>","<html>","<body>","<h1>My Heading</h1>","<p>My paragraph.</p>","</body>","</html>"]}]'? <div>  
+         <Button variant="primary" onClick={handleShow}> SUBMIT </Button>
+
+    <Modal show={show} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Woohoo!!!!</Modal.Title>
+      </Modal.Header>
+      <img src= "https://www.flaticon.com/svg/static/icons/svg/3159/3159066.svg" alt="css"/>
+      <Modal.Footer>
         
-        <img src= "https://www.flaticon.com/svg/static/icons/svg/3159/3159066.svg" alt="css"/>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClick}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={()=>{history.push('/ex4HTML')}}> 
-Go to the Next exersise          </Button>
-        </Modal.Footer>
-      </Modal>
+        <Button variant="primary" onClick={()=>{history.push('/ex4HTML')}}> Go to the Next exersise </Button>
+      </Modal.Footer>
+    </Modal></div>:<div>  
+         <Button variant="primary" onClick={handleShow}> SUBMIT </Button>
 
-        </div>
+    <Modal show={show} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Try Again!!!!</Modal.Title>
+      </Modal.Header>
+      <img src= "https://www.flaticon.com/svg/static/icons/svg/3159/3159020.svg" alt="css"/>
+      <Modal.Footer>
+      </Modal.Footer>
+    </Modal></div>}
 
-  
-  </div>
+      </div>
+
+
+</div></div>
+      
   
     )
 }
 
 export default DragNDrop
-
-
-
-
