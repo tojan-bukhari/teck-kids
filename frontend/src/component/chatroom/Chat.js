fb4fb73ff19d6107e7593950d1bec0009ca11107
Emanhusseinn <eman.hussein.jo@gmail.com>
2021-01-07 11:48:44 +0200

    csses4-done

--- frontend/src/component/chatroom/Chat.js
+++ frontend/src/component/chatroom/Chat.js
@@ -10,52 +10,61 @@ import Messages from './messages.js';
 //
 
 // import TextContainer from './TextContainer/TextContainer';
-// const ENDPOINT = 'localhost:8000';
+const ENDPOINT = 'localhost:8000';
 
 
 let socket;
+
 const Chat = ({ location }) => {
-    const [name, setName] = useState('');
-    const [room, setRoom] = useState('');
-    const [users, setUsers] = useState('');
-    const [message, setMessage] = useState('');
-    const [messages, setMessages] = useState([]);
-    useEffect(() => {
-      const { name, room } = queryString.parse(location.search);
-      socket = io();
-      setRoom(room);
-      setName(name)
-      socket.emit('join', { name, room }, (error) => {
-        if(error) {
-          alert(error);
-        }
-      });
-    }, [ location.search]);
-    useEffect(() => {
-      socket.on('message', message => {
-        setMessages(messages => [ ...messages, message ]);
-      });
-      socket.on("roomData", ({ users }) => {
-        setUsers(users);
-      });
-  }, []);
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
+  const [users, setUsers] = useState('');
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
+
+    socket = io(ENDPOINT);
+
+    setRoom(room);
+    setName(name)
+
+    socket.emit('join', { name, room }, (error) => {
+      if(error) {
+        alert(error);
+      }
+    });
+  }, [ENDPOINT, location.search]);
+  
+  useEffect(() => {
+    socket.on('message', message => {
+      setMessages(messages => [ ...messages, message ]);
+    });
+    
+    socket.on("roomData", ({ users }) => {
+      setUsers(users);
+    });
+}, []);
+
   const sendMessage = (event) => {
     event.preventDefault();
+
     if(message) {
       socket.emit('sendMessage', message, () => setMessage(''));
     }
   }
-  console.log(message,messages)
-    return(
-      <div className="outerContainer">
+
+  return (
+    <div className="outerContainer">
       <div className="container">
-          <InfoBar room={room} />    
+          <InfoBar room={room} />
           <Messages messages={messages} name={name} />
           <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
-          <div users={users}/>
-           </div>
+      </div>
+      {/* <TextContainer users={users}/> */}
     </div>
-    )
+  );
 }
-//
+
 export default Chat;
