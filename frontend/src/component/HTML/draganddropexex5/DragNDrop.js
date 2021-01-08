deb6ad1a49043c9d5f2f1bea429613874e83566b
Emanhusseinn <eman.hussein.jo@gmail.com>
2021-01-08 02:20:38 +0200

    html-ex3+ex4

--- frontend/src/component/HTML/draganddropexex5/DragNDrop.js
+++ frontend/src/component/HTML/draganddropexex5/DragNDrop.js
@@ -1,7 +1,7 @@
 import React, {useState, useRef} from 'react'
 import { Button, Modal} from 'react-bootstrap';
 import { useHistory } from "react-router-dom";
-// import './momo.css';
+import { Row, Col } from 'antd';
 
 
 
@@ -73,15 +73,59 @@ function DragNDrop({data}) {
 
     return (
       <div id= 'bbbbbb'>   
-                     <p  id='ididid'> ▶▶▶move the items from the Drag Bar and drop them in the right order inside the Drop bar◀◀◀</p>
+                     <p  id='ididid'> ▶▶▶Read the Following HTML code and check the classes and ides for each tag, then drop the right answer inside its right place ◀◀◀</p>
+<div>
+<div >
+<Row><Col  span={12}> 
+ <pre style={{background:"#383838	",  marginLeft: "60px;", borderStyle: "solid;", textAlign: "inherit", border:"20px"
+    }} >
+  <br></br>
+
+  &lt;!<span style={{color:"#C71585"}}>DOCTYPE</span><span style={{color:"#8B008B"}}>html</span>&gt;
+  <br></br>
+&lt;<span style={{color:"#C71585"}}>html</span> &gt;<br></br>
+&lt;<span style={{color:"#C71585"}}>body</span> &gt;<br></br>
+&lt;<span style={{color:"#C71585"}}>h4</span> &gt;<span style={{color:"#FFFAF0"}}>This is Heading4</span> &lt;/<span style={{color:"#C71585"}}>h4</span> &gt;    <br></br>
+&lt;<span style={{color:"#C71585"}}>h3</span> &gt;<span style={{color:"#FFFAF0"}}>This is Heading3</span> &lt;/<span style={{color:"#C71585"}}>h3</span> &gt;    <br></br>
+&lt;<span style={{color:"#C71585"}}>h6</span> &gt;<span style={{color:"#FFFAF0"}}>This is Heading6</span> &lt;/<span style={{color:"#C71585"}}>h6</span> &gt;    <br></br>
+&lt;<span style={{color:"#C71585"}}>h2</span> &gt;<span style={{color:"#FFFAF0"}}>This is Heading2</span> &lt;/<span style={{color:"#C71585"}}>h2</span> &gt;    <br></br>
+&lt;<span style={{color:"#C71585"}}>h5</span> &gt;<span style={{color:"#FFFAF0"}}>This is Heading5</span> &lt;/<span style={{color:"#C71585"}}>h5</span> &gt;    <br></br>
+&lt;<span style={{color:"#C71585"}}>h1</span> &gt;<span style={{color:"#FFFAF0"}}>This is Heading1</span> &lt;/<span style={{color:"#C71585"}}>h1</span> &gt;    <br></br>
+
+
+&lt;/<span style={{color:"#C71585"}}>body</span> &gt;<br></br>
+&lt;/<span style={{color:"#C71585"}}>html</span> &gt;<br></br>
+</pre>
+ </Col> 
+ <Col  id="apples" span={12}>
+   
+ <div  style={{borderStyle: 'solid',width:'500px',marginLeft:'90px', height:"276px" }}>
+<div >
+<h1 >This is Heading1 </h1 >    
+<h2 >This is Heading2 </h2 >    
+<h3 >This is Heading3 </h3 >    
+<h4 >This is Heading4 </h4 >    
+<h5 >This is Heading5 </h5 >    
+<h6 >This is Heading6 </h6 >    
+
+</div></div>
+   </Col>
+   </Row>
+   </div>
+
+
+
+</div>
+
+
 <div className="drag-n-drop">
       {list.map((grp, grpI) =>( 
-         <div key={grp.title}
+         <div key={grp.title, grp.title2, grp.title3 }
           className="dnd-group"
           onDragEnter={dragging && !grp.items.length?(e) => handleDragEnter(e,{grpI, itemI: 0}):null}
           
           >
-           <div className="group-title">{grp.title}</div>
+           <div className="group-title">{grp.title} </div>
             {grp.items.map((item, itemI) => (
               <div draggable   
                onDragStart={(e) => {handletDragStart(e, {grpI, itemI})} }
@@ -93,13 +137,19 @@ function DragNDrop({data}) {
             {item}
                 </div>
             ))}
+                        {grp.title2}
+                        {/* {item} */}
+                        {grp.title3 }
+
+                        
+
          </div>
       ))} 
       
       <div>
 
 
-      {localStorage.getItem('List')===  '[{"title":"Drag","items":[]},{"title":"Drop here","items":["<!DOCTYPE html>","<html>","<body>","<p>My paragraph.</p>","<h1>My Heading</h1>","</body>","</html>"]}]' ||localStorage.getItem('List')===  '[{"title":"Drag","items":[]},{"title":"Drop here","items":["<!DOCTYPE html>","<html>","<body>","<h1>My Heading</h1>","<p>My paragraph.</p>","</body>","</html>"]}]'? <div>  
+      {localStorage.getItem('List')===  '[{"title":"<body>","items":["<h1 >This is Heading1 </h1>","<h2 >This is Heading2 </h2 >"," <h3 >This is Heading3 </h3 > ","<h4 >This is Heading4 </h4 >","<h5 >This is Heading5 </h5 >","<h6 >This is Heading6 </h6 >"],"title2":"<body/>"},{"title":"<body>","items":[],"title2":"<body/>"}]'? <div>  
          <Button variant="primary" onClick={handleShow}> SUBMIT </Button>
 
     <Modal show={show} onHide={handleClose}>
@@ -109,7 +159,7 @@ function DragNDrop({data}) {
       <img src= "https://www.flaticon.com/svg/static/icons/svg/3159/3159066.svg" alt="css"/>
       <Modal.Footer>
         
-        <Button variant="primary" onClick={()=>{history.push('/ex3HTML')}}> Go to the Next exersise </Button>
+        <Button variant="primary" onClick={()=>{history.push('/ex5HTML')}}> Go to the Next exersise </Button>
       </Modal.Footer>
     </Modal></div>:<div>  
          <Button variant="primary" onClick={handleShow}> SUBMIT </Button>
